b291cf44de3257eede3f53bbb33d1dc979b1d366
Tony Arnold <tony@thecocoabots.com>
2011-02-01 11:18:00 +1100

    Making Code Mirror compatible with Symphony 2.2.

--- extension.driver.php
+++ extension.driver.php
@@ -1,34 +1,43 @@
 <?php
  
-	Class extension_codemirror extends Extension{
+	Class extension_codemirror extends Extension {
  
 		public function about(){
-			return array('name' => 'Enable Codemirror',
-						 'version' => '1.2',
-						 'release-date' => '2009-12-01',
-						 'author' => array('name' => 'Ole A. E.',
-							        'website' => 'http://oleae.no',
-							        'email' => 'post@oleae.no')
-				 		);
+			return array(
+			  'name'         => 'Codemirror',
+        'version'      => '1.2.1',
+        'release-date' => '2011-02-01',
+        'description'  => 'Syntax colouring for Symphony\'s backend.',
+        'author'       => array(
+          'name'    => 'Ole A. E.',
+          'website' => 'http://oleae.no',
+          'email'   => 'post@oleae.no')
+        );
 		}
  
  
 		public function getSubscribedDelegates() {
 			return array(
 				array(
-					'page'		=> '/backend/',
+					'page'		  => '/backend/',
 					'delegate'	=> 'InitaliseAdminPageHead',
-					'callback'	=> 'initaliseAdminPageHead'
+					'callback'	=> '__appendAdminPageAssets'
 				)
 			);
 		}
+		
+		protected $adminHeadersAppended = false;
  
-		public function initaliseAdminPageHead($context) {
-			$page = $context['parent']->Page;
+		public function __appendAdminPageAssets($context) {   
+		  $engine = $context['parent'];
+		     
+      if (!$this->adminHeadersAppended && $engine) {
+        $engine->Page->addStylesheetToHead(URL . '/extensions/codemirror/assets/css/codemirror.css', 'screen', 3066705);
+        $engine->Page->addScriptToHead(URL . '/extensions/codemirror/assets/js/codemirror.js', 3066704);
+        $engine->Page->addScriptToHead(URL . '/extensions/codemirror/assets/js/init.js', 3066703);
         
-		   $page->addStylesheetToHead(URL . '/extensions/codemirror/assets/css/codemirror.css', 'screen', 3066705);
-			$page->addScriptToHead(URL . '/extensions/codemirror/assets/js/codemirror.js', 3066704);
-			$page->addScriptToHead(URL . '/extensions/codemirror/assets/js/init.js', 3066703);
+			  $this->adminHeadersAppended = true;
+			}
 		}
  
 	}
