63be9e8f41023128ebd114e18d141e88d75e7e66
George Petsagourakis <petsagouris@gmail.com>
2012-06-01 21:49:32 +0300

    Updating to CodeMirror 2.25, works in 2.3, added extension.meta.xml for tracking.

--- extension.driver.php
+++ extension.driver.php
@@ -1,45 +1,43 @@
 <?php
  
 	Class extension_codemirror extends Extension {
- 
+
 		public function about(){
 			return array(
-			  'name'         => 'Codemirror',
-        'version'      => '1.2.1',
-        'release-date' => '2011-02-01',
-        'description'  => 'Syntax colouring for Symphony\'s backend.',
-        'author'       => array(
-          'name'    => 'Ole A. E.',
-          'website' => 'http://oleae.no',
-          'email'   => 'post@oleae.no')
-        );
-		}
- 
- 
-		public function getSubscribedDelegates() {
-			return array(
-				array(
-					'page'		  => '/backend/',
-					'delegate'	=> 'InitaliseAdminPageHead',
-					'callback'	=> '__appendAdminPageAssets'
+				'name' => 'Codemirror',
+				'version' => '1.2.1',
+				'release-date' => '2011-02-01',
+				'description' => 'Syntax colouring for Symphony\'s backend.',
+				'author' => array(
+					'name' => 'Ole A. E.',
+					'website' => 'http://oleae.no',
+					'email' => 'post@oleae.no'
 				)
 			);
 		}
-		
+
+
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page' => '/backend/',
+					'delegate' => 'InitaliseAdminPageHead',
+					'callback' => '__appendAdminPageAssets'
+				)
+			);
+		}
+
 		protected $adminHeadersAppended = false;
- 
+
 		public function __appendAdminPageAssets($context) {   
-		  $engine = $context['parent'];
-		     
-      if (!$this->adminHeadersAppended && $engine) {
-        $engine->Page->addStylesheetToHead(URL . '/extensions/codemirror/assets/css/codemirror.css', 'screen', 3066705);
-        $engine->Page->addScriptToHead(URL . '/extensions/codemirror/assets/js/codemirror.js', 3066704);
-        $engine->Page->addScriptToHead(URL . '/extensions/codemirror/assets/js/init.js', 3066703);
-        
-			  $this->adminHeadersAppended = true;
+			if (!$this->adminHeadersAppended && Administration::instance()) {
+				Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/codemirror/assets/css/codemirror.css', 'screen', 3066704);
+				Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/codemirror/assets/css/codemirror-symphony.css', 'screen', 3066706);
+				Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/codemirror/assets/css/codemirror-elegant.css', 'screen', 3066705);
+				Administration::instance()->Page->addScriptToHead(URL . '/extensions/codemirror/assets/js/codemirror-compressed.js', 3066703);
+				Administration::instance()->Page->addScriptToHead(URL . '/extensions/codemirror/assets/js/init.js', 3066702);
+				$this->adminHeadersAppended = true;
 			}
 		}
- 
+
 	}
- 
-?>
